d63e8e375ec97f2890408da6ee9af67629d585c4
AravKotwal18 <aravkotwal1604@gmail.com>
2026-08-09 15:44:12 +0530

    Update ITriageService interface

--- Interfaces/ITriageService.cs
+++ Interfaces/ITriageService.cs
@@ -1,9 +1,11 @@
-using ArogyaPulse.Api.Models;
+using ArogyaPulse.Api.DTOs;
+
 namespace ArogyaPulse.Api.Interfaces
 {
     public interface ITriageService
     {
         int CalculateScore(string bp, int spO2, double temp, int glucose, bool isPregnant);
         string GetRiskLevel(int score);
+        TriageResultDto EvaluateTriage(string bp, int spO2, double temp, int glucose, bool isPregnant);
     }
 }
