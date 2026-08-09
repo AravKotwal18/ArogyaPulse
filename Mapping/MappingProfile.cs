4ba7b5946f7a5d0e3ddebaf6bc6dcee38391c935
AravKotwal18 <aravkotwal1604@gmail.com>
2026-08-09 14:13:36 +0530

    Wire up DI container with repository, services, and AutoMapper

---
+++ Mapping/MappingProfile.cs
@@ -0,0 +1,29 @@
+using AutoMapper;
+using ArogyaPulse.Api.Models;
+using ArogyaPulse.Api.DTOs;
+namespace ArogyaPulse.Api.Mapping
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Patient, PatientResponseDto>()
+                .ForMember(dest => dest.Vitals, opt => opt.MapFrom(src => new VitalsDto
+                {
+                    Bp = src.Bp,
+                    SpO2 = src.SpO2,
+                    Temp = src.Temp,
+                    Glucose = src.Glucose
+                }));
+            CreateMap<PatientCreateDto, Patient>()
+                .ForMember(dest => dest.Bp, opt => opt.MapFrom(src => src.Vitals.Bp))
+                .ForMember(dest => dest.SpO2, opt => opt.MapFrom(src => src.Vitals.SpO2))
+                .ForMember(dest => dest.Temp, opt => opt.MapFrom(src => src.Vitals.Temp))
+                .ForMember(dest => dest.Glucose, opt => opt.MapFrom(src => src.Vitals.Glucose))
+                .ForMember(dest => dest.Id, opt => opt.Ignore())
+                .ForMember(dest => dest.RiskScore, opt => opt.Ignore())
+                .ForMember(dest => dest.RiskLevel, opt => opt.Ignore())
+                .ForMember(dest => dest.Timestamp, opt => opt.Ignore());
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ArogyaPulse.Api.Data;
+using ArogyaPulse.Api.Interfaces;
 using ArogyaPulse.Api.Repositories;
 using ArogyaPulse.Api.Services;
 using ArogyaPulse.Api.Mapping;
